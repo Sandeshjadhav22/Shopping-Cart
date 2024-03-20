78a50d1613300f1c63e0adfda4b6c843052cfa1b
sandy <sandeshjadhav2002@gmail.com>
2024-03-20 23:12:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
